1bc6411f05e0972d5f808804410a7fbf602bf876
zhuchao <1341772976@qq.com>
2015-07-12 05:11:24 +0000

    First Version

---
+++ app/src/main/java/com/zhuchao/freetime/Welcome.java
@@ -0,0 +1,31 @@
+package com.zhuchao.freetime;
+
+import android.content.SharedPreferences;
+import android.graphics.PixelFormat;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class Welcome extends ActionBarActivity implements Runnable{
+
+    private boolean isFirst;//check whether use this software firstly.
+    private final String setting="Setting";//get base setting name
+    private SharedPreferences preferences;//get setting
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFormat(PixelFormat.RGBA_8888);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DITHER);
+        setContentView(R.layout.activity_welcome);
+    }
+    @Override
+    public void run() {
+        preferences=getSharedPreferences(setting,MODE_PRIVATE);
+        isFirst=preferences.getBoolean("STARTFIRST",true);
+        if(isFirst){//use firstly
+            preferences.edit().putBoolean("STARTFIRST",false);
+        }else{
+
+        }
+    }
+}
