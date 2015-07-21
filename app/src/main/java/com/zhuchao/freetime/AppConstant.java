9dae5a5a0ab41f75f5f6b745790e106e542ffc43
zhuchao <1341772976@qq.com>
2015-07-21 11:26:45 +0000

    2015.7.19

--- app/src/main/java/com/zhuchao/freetime/AppConstant.java
+++ app/src/main/java/com/zhuchao/freetime/AppConstant.java
@@ -4,5 +4,5 @@ package com.zhuchao.freetime;
  * Created by Henry on 2015/7/17.
  */
 public class AppConstant {
-    public static String APP_ID_QQ = "1104778012";
+    public static String APP_ID_QQ = "222222";
 }
