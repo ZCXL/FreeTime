9698677c600ecd7787d2fd02e3c668fa7ec3a5b6
Henry <Henry_lmz@163.com>
2015-07-27 17:27:33 +0800

    2015.7.25-20:20-AutoIntentService with AlarmManager

--- app/src/main/java/com/zhuchao/freetime/Login.java
+++ app/src/main/java/com/zhuchao/freetime/Login.java
@@ -212,7 +212,7 @@ public class Login extends Activity implements View.OnClickListener {
                 Log.d(TAG_Weibo, "----mAccessToken----" + mAccessToken.toString());
                 AccessTokenKeeper.writeAccessToken(getApplicationContext(), mAccessToken);
                 mAccessToken = AccessTokenKeeper.readAccessToken(getApplicationContext());
-                Log.e(TAG_Weibo, "---mAccessToken---" + mAccessToken.toString());
+                Log.d(TAG_Weibo, "---mAccessToken---" + mAccessToken.toString());
                 //get userInfo' json for weibo
                 UsersAPI mUsersAPI = new UsersAPI(getApplicationContext(),mAppKeyWeibo,mAccessToken);
                 uid = Long.parseLong(mAccessToken.getUid());
@@ -220,7 +220,7 @@ public class Login extends Activity implements View.OnClickListener {
                     @Override
                     public void onComplete(String response) {
                         if (!TextUtils.isEmpty(response)){
-                            Log.e(TAG_Weibo,"---user s---"+response);
+                            Log.d(TAG_Weibo,"---user s---"+response);
                             Message message=new Message();
                             message.what=3;
                             message.obj=response;
